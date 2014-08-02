2f908f147a9c9e06733b89bbf92688864a9c5e51
c-ice <lukas.chudy@softec.sk>
2014-08-02 15:42:02 +0200

    merge

--- www/ScreenOrientation.js
+++ www/ScreenOrientation.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var argscheck = require('cordova/argscheck'),
 utils = require('cordova/utils'),
 exec = require('cordova/exec');
@@ -20,28 +19,4 @@ ScreenOrientation.changeScreenOrientationTo = function(orientation) {
 
 
 module.exports = ScreenOrientation;
-=======
-
-               
-var argscheck = require('cordova/argscheck'),
-	utils = require('cordova/utils'),
-     	exec = require('cordova/exec');
-               
-               
-               
-        var ScreenOrientation = function(){
-        };
-               
-               
-ScreenOrientation.changeScreenOrientationTo = function(orientation) {
-                exec( function(){console.log("succesfully changed orientation.")},
-                    function(){console.log("failed changed orientation.")},
-						   "ScreenOrientation",
-						   "screenorientationFunction",
-						   [orientation]);
-               };
-
-               
-               module.exports = ScreenOrientation;
->>>>>>> FETCH_HEAD
 
