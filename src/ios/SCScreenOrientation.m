5c893ace59151dcccc563cf6deb32137206595c4
Lukas Chudy <lukas.chudy@softec.sk>
2015-03-19 18:58:17 +0100

    Update SCScreenOrientation.m

--- src/ios/SCScreenOrientation.m
+++ src/ios/SCScreenOrientation.m
@@ -2,22 +2,6 @@
 
 @implementation SCScreenOrientation
 
-- (CDVPlugin*)initWithWebView:(UIWebView*)theWebView
-{
-    self = [super initWithWebView:theWebView];
-    if (self) {
-        [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(onOrientationWillChange) name:UIApplicationWillChangeStatusBarOrientationNotification object:nil];
-    }
-    
-    return self;
-}
-
-- (void) onOrientationWillChange {
-    NSString *jsCall = @"window.onOrientationWillChange && window.onOrientationWillChange()";
-    
-    [self.webView stringByEvaluatingJavaScriptFromString:jsCall];
-}
-
 - (void) changeOrientation:(CDVInvokedUrlCommand*)command {
     
     
